f979384d388979c701fe96b77a74d6c437ccb2b3
Marie-Elise Lecoq <MEGAUTIER@Marie-Elises-MacBook-Air.local>
2013-11-24 18:53:37 +0100

    Reception du json

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
-import play.*;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import play.mvc.*;
 
-import views.html.*;
 
 public class Application extends Controller {
 
+	@With(CorsWrapper.class)
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+		JsonNode json = request().body().asJson();
+		ObjectMapper mapper = new ObjectMapper();	 
+		try { 
+			// read from file, convert it to user class
+			SearchParser search = mapper.readValue(json.traverse(), SearchParser.class);	 
+			// display to console
+		    for(int k=0; k<search.getScientificName().length; k++)
+		        System.out.println("Scientific name = " + search.getScientificName()[k]);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ok("blblbl");
     }
 
 }
