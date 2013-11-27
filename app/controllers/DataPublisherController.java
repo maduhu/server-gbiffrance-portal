a91011bc0b76cc32f63ee983842ef5dc1e52c43d
Marie-Elise Lecoq <MEGAUTIER@Marie-Elises-MacBook-Air.local>
2013-11-27 16:05:22 +0100

    ajout fichier ES + Play

---
+++ app/controllers/DataPublisherController.java
@@ -0,0 +1,61 @@
+package controllers;
+
+import java.util.ArrayList;
+
+import models.DataPublisher;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.search.SearchHit;
+
+import com.github.cleverage.elasticsearch.IndexClient;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.With;
+
+public class DataPublisherController extends Controller {
+
+	@SuppressWarnings("unchecked")
+	@With(CorsWrapper.class)
+	public static Result searchAll() {
+
+//		 GetResponse response =
+//		 IndexClient.client.prepareGet("gbiffrance-harvest", "DataPublisher",
+//		 "3")
+//		 .execute()
+//		 .actionGet();
+
+		SearchResponse response = IndexClient.client
+				.prepareSearch("gbiffrance-harvest")
+				.setTypes("DataPublisher")
+				.execute()
+				.actionGet();
+
+		Long nbHits = response.getHits().getTotalHits();
+		System.out.println(nbHits);
+		ArrayList<DataPublisher> dataPublisherList = new ArrayList<DataPublisher>();
+
+		for (SearchHit hit : response.getHits()) {
+			DataPublisher dataPublisher = new DataPublisher();
+//			dataPublisher.setId(hit.getSource()
+//					.get("_id").);
+			dataPublisher.setClassName((String) hit.getSource()
+					.get("className"));
+			dataPublisher.setName((String) hit.getSource()
+					.get("name"));
+			dataPublisher.setDescription((String) hit.getSource()
+					.get("description"));
+			dataPublisher.setAdministrativeContact((String) hit.getSource()
+					.get("administrativeContact"));
+			dataPublisher.setTechnicalContact((String) hit.getSource()
+					.get("technicalContact"));
+			dataPublisher.setTags((ArrayList<String>) hit.getSource()
+					.get("tags"));
+			dataPublisherList.add(dataPublisher);
+		}
+		
+		return ok(nbHits.toString());
+//		return ok(Response.toString());
+	}
+
+}
